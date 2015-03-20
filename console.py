ca64a5cd016589c2c47a163b7b00e2a43888f777
jarulraj <jarulraj@cs.cmu.edu>
2015-03-20 03:34:04 -0400

    Added seq scan, count, limit and pretty printer

--- console.py
+++ console.py
@@ -7,28 +7,87 @@ import psycopg2
 import getpass
 import sys
 import traceback
+import pprint
 
 from numbers import Number
+from tabulate import tabulate
+
+# DB Connection
+db_conn = None    
+
+tables = set()
+keywords = set(['limit', "only", "just",
+                'count', "many", "number"])
+
+
+column_to_table = {}
+table_to_column = {}
+
+limit = False
+limit_cnt = 0
+
+count = False
+
+## ============================================================================================
+## SEQ SCAN
+## ============================================================================================
+
+def execute_query(query):
+    "Execute the query on the given connection"
+    
+    print("SQL Query :: " + query)
+    
+    try:
+        cursor = db_conn.cursor()                        
+        cursor.execute(query)
+
+        columns = [desc[0] for desc in cursor.description]        
+                
+        rows = cursor.fetchall();               
+        if rows is not None:
+            print (tabulate(rows, headers=columns, tablefmt="psql"))
+
+    except Exception:
+        print (traceback.format_exc())
+        print ("Unable to perform operation. Please try again.")
+        if db_conn is not None:
+            db_conn.rollback()
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+def get_limit_string():
+    "Get LIMIT string"
+    
+    if limit is True:
+        limit_string = " LIMIT " + str(limit_cnt)
+    else:
+        limit_string = " "
+
+    return limit_string
+
+def run_seq_scan(tokens):
+    "Execute seq scan"
+
+    for token in tokens:
+        if token in tables:
+                
+            if count:
+                execute_query("SELECT COUNT(*) FROM " + token + get_limit_string())
+            else:                
+                execute_query("SELECT * FROM " + token + get_limit_string())
+            break
 
 ## ============================================================================================
 ## Command Interpreter
 ## ============================================================================================
-
-# DB Connection
-db_conn = None;    
-
-tables = set();
-keywords = set();
-
-column_to_table = {};
-table_to_column = {};
 
 class query(cmd.Cmd):
     intro = """English to SQL translator.\nType \"help\" for help.\n"""
     
     ruler = '-'
     prompt = 'query=# '
-                
 
 ## ============================================================================================
 ## Basic commands
@@ -54,21 +113,21 @@ class query(cmd.Cmd):
 ## Pick database
 ## ============================================================================================
     
-    def do_db(self, line):
+    def do_db(self, line = "parallels"):
         "Pick the database to analyze"
         print ("Looking up database : ", line)
 
         try:
             db = line
-            c_user = raw_input('Enter username: ')
-            c_password = getpass.getpass(prompt="Enter password : ")
-            c_host = raw_input("Enter hostname : ") 
-            c_port = raw_input("Enter port : ")
+            #c_user = raw_input('Enter username: ')
+            #c_password = getpass.getpass(prompt="Enter password : ")
+            #c_host = raw_input("Enter hostname : ") 
+            #c_port = raw_input("Enter port : ")
     
-            if not c_host:
-                c_host = 'localhost'
-            if not c_port:
-                c_port = '5432'
+            #if not c_host:
+            c_host = 'localhost'
+            #if not c_port:
+            c_port = '5432'
                 
             c_user = "parallels";
             c_password = "parallels"
@@ -164,7 +223,7 @@ class query(cmd.Cmd):
 ## Parse
 ## ============================================================================================
         
-    def do_parse(self,line):
+    def do_p(self,line):
         "Parse a query"
 
         def isfloat(value):
@@ -188,6 +247,11 @@ class query(cmd.Cmd):
         print("Tokens :: " + str(tokens))
 
         filtered_tokens = set()
+          
+        table_ref = 0
+        column_ref = 0
+        point_lookup = 0
+        range_lookup = 0
             
         for token in tokens:
             if token in keywords or isfloat(token) or isquoted(token):                
@@ -196,7 +260,42 @@ class query(cmd.Cmd):
                                 
                 filtered_tokens.add(token)        
         
+                if token in tables:
+                    table_ref = table_ref + 1
+                
         print("Filtered Tokens :: " + str(filtered_tokens))
+
+        # Set LIMIT
+        global limit;
+        global limit_cnt;
+        
+        limit = False
+        if "limit" in tokens or "only" in tokens or "just" in tokens:
+            limit = True
+
+            try:
+                if "limit" in tokens:
+                    limit_cnt = tokens[tokens.index("limit") + 1]
+                elif "only" in tokens:
+                    limit_cnt = tokens[tokens.index("only") + 1]
+                else:
+                    limit_cnt = tokens[tokens.index("just") + 1]
+                    
+                limit = limit_cnt.isdigit()
+                
+            except ValueError:
+                limit = False;
+
+        # Set COUNT
+        global count;
+        
+        count = False
+        if "count" in tokens or "many" in tokens or "number" in tokens:
+            count = True
+            
+        # Set sequential scan
+        if table_ref == 1:
+            run_seq_scan(filtered_tokens);
         
         
 if __name__ == '__main__':
