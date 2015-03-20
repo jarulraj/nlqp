d874172bca3b4559a8fc7d6e87d74891ce9fb4b3
jarulraj <jarulraj@cs.cmu.edu>
2015-03-19 20:30:30 -0400

    Added basic parsing

--- console.py
+++ console.py
@@ -8,16 +8,20 @@ import getpass
 import sys
 import traceback
 
+from numbers import Number
+
 ## ============================================================================================
-# # Command Interpreter
+## Command Interpreter
 ## ============================================================================================
 
 # DB Connection
 db_conn = None;    
 
-dbs = [];
-db_tables = [];
+tables = set();
+keywords = set();
 
+column_to_table = {};
+table_to_column = {};
 
 class query(cmd.Cmd):
     intro = """English to SQL translator.\nType \"help\" for help.\n"""
@@ -25,7 +29,11 @@ class query(cmd.Cmd):
     ruler = '-'
     prompt = 'query=# '
                 
-    # Add shell support
+
+## ============================================================================================
+## Basic commands
+## ============================================================================================
+                
     def do_shell(self, line):
         "Run a shell command"
         print ("Executing shell command :", line)
@@ -41,6 +49,10 @@ class query(cmd.Cmd):
     def do_exit(self, line):
         "Quit console"
         return True
+
+## ============================================================================================
+## Pick database
+## ============================================================================================
     
     def do_db(self, line):
         "Pick the database to analyze"
@@ -62,82 +74,130 @@ class query(cmd.Cmd):
             c_password = "parallels"
             
             global db_conn;            
+            cursor = None
+            
             db_conn = psycopg2.connect(database=db, user=c_user, password=c_password, 
                                     host=c_host, port=c_port)
+
+                        
+            ## POPULATE TABLES
             
-            print(db_conn)
+            cursor = db_conn.cursor()                        
+            cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+
+            global tables;
+            rows = cursor.fetchall();                
             
+            for row in rows:
+                table = row[0]
+                tables.add(table)
+                keywords.add(table)
+
+            ## POPULATE COLUMNS
+
+            global table_to_column;
+            global column_to_table;
+
+            for table in tables:
+                cursor.execute("SELECT * FROM " + table +" LIMIT 0")
+                columns = [desc[0] for desc in cursor.description]
+                
+                table_to_column[table] = columns;
+                
+                for column in columns:
+                    column_to_table[column] = table;
+                    keywords.add(column)
+                                                            
         except Exception:
             print (traceback.format_exc())
             print ("Unable to perform operation. Please try again.")
-            
+            if db_conn is not None:
+                db_conn.rollback()
+
+        finally:
+            if cursor is not None:
+                cursor.close()
+
+
+## ============================================================================================
+## List tables
+## ============================================================================================
                                 
     def do_list_tables(self, line):
         "List the tables in the database"
 
-        global db_conn;
         if db_conn == None:
             print ("Pick database first")        
         else:
-            cursor = db_conn.cursor()            
-            
-            try:            
-                cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+            print (tables)
 
-                global db_tables;
-                rows = cursor.fetchall();                
-                
-                for row in rows:
-                    db_tables.append(row[0])
-
-                print (db_tables)
-                
-            except Exception:
-                print (traceback.format_exc())
-                print ("Unable to perform operation. Please try again.")
-                db_conn.rollback()
-
-            finally:
-                if cursor is not None:
-                    cursor.close()
+## ============================================================================================
+## List columns
+## ============================================================================================
             
     def do_list_columns(self, line):
         "List the columns in the table"        
-
-        table = line;
         
-        global db_conn;
         if db_conn == None:
             print ("Pick database first")        
         else:
-            cursor = db_conn.cursor()            
-
-            try:                        
-                cursor.execute("SELECT * FROM " + table +" LIMIT 0")
-                column_names = [desc[0] for desc in cursor.description]
-                print (column_names)
-    
+            table = line;
+            try:
+                print (table_to_column[table])
             except Exception:
                 print (traceback.format_exc())
                 print ("Unable to perform operation. Please try again.")
-                db_conn.rollback()
-
-            finally:
-                if cursor is not None:
-                    cursor.close()
+ 
 
     def complete_list_columns(self, text, line, start_index, end_index):        
         "Auto complete list column command"
-        global db_tables;
 
         if text:
             return [
-                db_table for db_table in db_tables
-                if db_table.startswith(text)
+                table for table in tables
+                if table.startswith(text)
             ]
         else:
-            return db_tables
+            return tables
+
+## ============================================================================================
+## Parse
+## ============================================================================================
+        
+    def do_parse(self,line):
+        "Parse a query"
+
+        def isfloat(value):
+            try:
+              float(value)
+              return True
+            except ValueError:
+              return False        
+                  
+        def isquoted(value):
+            if value.startswith("'") and value.endswith("'"):          
+                return True;
+            else:
+                if value.startswith("\"") and value.endswith("\""):          
+                    return True;            
+            return False;
+                    
+        # Tokenize
+        tokens = line.split(' ')
+        
+        print("Tokens :: " + str(tokens))
+
+        filtered_tokens = set()
             
+        for token in tokens:
+            if token in keywords or isfloat(token) or isquoted(token):                
+                if(isquoted(token)):
+                    token = token[1:-1]
+                                
+                filtered_tokens.add(token)        
+        
+        print("Filtered Tokens :: " + str(filtered_tokens))
+        
         
 if __name__ == '__main__':
     query_cmd = query()
